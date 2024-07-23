607073b3aeeb1672409e32f5f6ec60d22b5e4a46
Apostolis Mpostanis <mpoapostolis@ip-172-31-254-45.eu-central-1.compute.internal>
2024-07-24 00:04:48 +0300

    better dialogue

--- src/components/dialogue/index.tsx
+++ src/components/dialogue/index.tsx
@@ -5,43 +5,41 @@ import { useWindupString } from "windups"
 export function Dialogue() {
   const store = useStore()
   const [text] = useWindupString(store?.dialog?.content ?? "", {
-    pace: (char: string) => (char === " " ? 50 : 30),
+    pace: (char) => (char === " " ? 50 : 30),
   })
+
+  const handleDialogueClick = () => {
+    store.setDialog(undefined)
+  }
+
+  const { dialog } = store
+  const showDialogue = dialog ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"
+
   return (
     <div
-      onClick={() => store.setDialog(undefined)}
-      className={cn("h-screen w-screen fixed flex md:items-end items-center p-4 top-0 left-0  z-50", {
-        "pointer-events-none opacity-0": !store.dialog,
-        "pointer-events-auto opacity-100": store.dialog,
-      })}
+      onClick={handleDialogueClick}
+      className={cn("h-screen w-screen fixed flex md:items-end items-center p-4 top-0 left-0 z-50", showDialogue)}
     >
-      <div className="w-full p-4 md:m-8 text-gray-300 rounded-lg bg-black flex ">
-        <div className="avatar mr-8">
-          <div className="w-32 h-fit rounded">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+      <div className="w-full p-4 md:m-8 text-gray-300 rounded-lg bg-black min-h-20">
+        <div className="flex w-full">
+          <div className="avatar mr-2 mb-4">
+            <div className="md:w-32 w-20 h-fit rounded">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" alt="Avatar" />
+            </div>
           </div>
+          <div className="text-xl w-full">{text}</div>
         </div>
         <div className="grid gap-4 w-full">
-          <div className="text-xl w-full ">{text}</div>
-          <div
-            className={cn("divider", {
-              hidden: !store.dialog?.divider,
-            })}
-          >
-            {store.dialog?.divider}
-          </div>
-
-          <div
-            className={cn("grid gap-2", {
-              hidden: !store.dialog?.choices,
-            })}
-          >
-            {store.dialog?.choices?.map((choice, i) => (
-              <button key={i} onClick={choice?.onSelect} className="btn w-full btn-sm btn-outline">
-                {choice.label}
-              </button>
-            ))}
-          </div>
+          {dialog?.divider && <div className="divider">{dialog.divider}</div>}
+          {dialog?.choices && (
+            <div className="grid gap-2">
+              {dialog.choices.map((choice, i) => (
+                <button key={i} onClick={choice.onSelect} className="btn w-full btn-sm btn-outline">
+                  {choice.label}
+                </button>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
